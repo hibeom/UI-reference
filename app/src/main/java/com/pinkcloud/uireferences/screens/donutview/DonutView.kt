7bb7c314b84a1d28a0078838c336a5e11ec11eb6
hibeom <hbkim1293@gmail.com>
2021-10-02 12:03:49 +0900

    add donutview

---
+++ app/src/main/java/com/pinkcloud/uireferences/screens/donutview/DonutView.kt
@@ -0,0 +1,12 @@
+package com.pinkcloud.uireferences.screens.donutview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class DonutView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+
+}
